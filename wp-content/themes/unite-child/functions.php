ea8d8966cc0aabfb63b6947f82359d56b5f8674a
Winnipass <adams4lyf@gmail.com>
2019-03-15 14:13:12 +0000

    ft(taxonomy): added country year and actor taxonomies

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -75,7 +75,7 @@ function create_film_genre_taxonomy() {
  
     $labels = array(
       'name' => _x('Genres', 'film genre'),
-      'singular_name' => _x('Genre', 'taxonomy singular name'),
+      'singular_name' => _x('Genre', 'film singular name'),
       'search_items' =>  __('Search Genres'),
       'all_items' => __('All Genres'),
       'parent_item' => '',
@@ -101,3 +101,112 @@ function create_film_genre_taxonomy() {
 }
 
 add_action('init', 'create_film_genre_taxonomy', 0);
+
+/**
+ * Register our film taxonomy for our Films.
+ * 
+ * @return null
+ */
+function create_film_country_taxonomy() {
+ 
+    $labels = array(
+      'name' => _x('Countries', 'film country'),
+      'singular_name' => _x('Country', 'country singular name'),
+      'search_items' =>  __('Search Countries'),
+      'all_items' => __('All Countries'),
+      'parent_item' => '',
+      'parent_item_colon' => '',
+      'edit_item' => __('Edit Country'), 
+      'update_item' => __('Update Country'),
+      'add_new_item' => __('Add New Country'),
+      'new_item_name' => __('New Country Name'),
+      'menu_name' => __('Countries'),
+    );
+    
+    $args =  array(
+        'labels' => $labels,
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => true,
+        'show_tagcloud' => true,
+    );
+
+    register_taxonomy('country', array('film'), $args);
+}
+
+add_action('init', 'create_film_country_taxonomy', 0);
+
+
+/**
+ * Register our year taxonomy for our Films.
+ * 
+ * @return null
+ */
+function create_film_year_taxonomy() {
+ 
+    $labels = array(
+      'name' => _x('Years', 'film year'),
+      'singular_name' => _x('Year', 'year singular name'),
+      'search_items' =>  __('Search Year'),
+      'all_items' => __('All Years'),
+      'parent_item' => '',
+      'parent_item_colon' => '',
+      'edit_item' => __('Edit Year'), 
+      'update_item' => __('Update Year'),
+      'add_new_item' => __('Add New Year'),
+      'new_item_name' => __('New Year Name'),
+      'menu_name' => __('Year'),
+    );
+    
+    $args =  array(
+        'labels' => $labels,
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => true,
+        'show_tagcloud' => true,
+    );
+
+    register_taxonomy('year', array('film'), $args);
+}
+
+add_action('init', 'create_film_year_taxonomy', 0);
+
+/**
+ * Register our actor taxonomy for our Films.
+ * 
+ * @return null
+ */
+function create_film_actor_taxonomy() {
+ 
+    $labels = array(
+      'name' => _x('Actors', 'film actor'),
+      'singular_name' => _x('Actor', 'actor singular name'),
+      'search_items' =>  __('Search Actor'),
+      'all_items' => __('All Actors'),
+      'parent_item' => '',
+      'parent_item_colon' => '',
+      'edit_item' => __('Edit Actor'), 
+      'update_item' => __('Update Actor'),
+      'add_new_item' => __('Add New Actor'),
+      'new_item_name' => __('New Actor Name'),
+      'menu_name' => __('Actors'),
+    );
+    
+    $args =  array(
+        'labels' => $labels,
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => true,
+        'show_tagcloud' => true,
+    );
+
+    register_taxonomy('actor', array('film'), $args);
+}
+
+add_action('init', 'create_film_actor_taxonomy', 0);
