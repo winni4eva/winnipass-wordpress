7563f741d3d8ccafd4d9ff9e207e2a08662c3cef
Winnipass <adams4lyf@gmail.com>
2019-03-15 16:40:37 +0000

    en(view): added custom taxonomies and meta field data to film page

--- wp-content/themes/unite-child/template-content.php
+++ wp-content/themes/unite-child/template-content.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
-
+            
 			<?php unite_post_nav(); ?>
 
 			<?php
